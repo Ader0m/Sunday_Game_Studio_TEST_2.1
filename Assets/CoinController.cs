e7cb6ee219b486b7203aad5354742b10d3164cfc
Ader0m <k1selka@yandex.ru>
2023-07-02 18:44:42 +0300

    init

---
+++ Assets/CoinController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinController : MonoBehaviour
+{
+    [SerializeField] private Scrollbar _scrollbarXRotation;
+    [SerializeField] private Scrollbar _scrollbarXRotation;
+    [SerializeField] private Scrollbar _scrollbarXRotation;
+
+    void Update()
+    {
+        
+    }
+}
